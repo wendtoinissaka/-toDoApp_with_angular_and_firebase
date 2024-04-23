0520359499d1d63a09cbb8c86a1be89b90c4cc00
wendtoinissaka <ouedraogowendtoinissaka@gmail.com>
2024-04-23 18:00:44 +0000

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+
+        apiKey: "AIzaSyCVjY4LOrFOZAktZm5X7eO7ZvTMgEh9Pxg",
+      
+        authDomain: "angular-todoapp-afbf2.firebaseapp.com",
+      
+        projectId: "angular-todoapp-afbf2",
+      
+        storageBucket: "angular-todoapp-afbf2.appspot.com",
+      
+        messagingSenderId: "690827969854",
+      
+        appId: "1:690827969854:web:4d0319719b3ab74498186f"
+      
+      }
+      
+};
